339d2bed877570c2404020378be2f38375749ddb
kemal <kemalxyz@gmail.com>
2017-09-03 05:38:58 +0300

    Adds network page resctriction message if user is not logged in

--- app/views/_networks/networks.controller.js
+++ app/views/_networks/networks.controller.js
@@ -9,12 +9,12 @@
 		.module( 'com.missofis.ontheair' )
 		.controller( 'NetworksCtrl', NetworksCtrl );
 
-	NetworksCtrl.$inject = [ '$log', 'OnTheAirFirebaseDatabase', '$q', '$routeParams', 'TMDbDiscover', 'TMDbNetworks' ];
+	NetworksCtrl.$inject = [ '$log', 'OnTheAirFirebaseDatabase', '$q', '$routeParams', 'TMDbDiscover', 'TMDbNetworks', 'OnTheAirUtils', '$scope', '$mdDialog', '$location', '$mdToast' ];
 
 	/**
 	 * Networks controller
 	 */
-	function NetworksCtrl( $log, OnTheAirFirebaseDatabase, $q, $routeParams, TMDbDiscover, TMDbNetworks ) {
+	function NetworksCtrl( $log, OnTheAirFirebaseDatabase, $q, $routeParams, TMDbDiscover, TMDbNetworks, OnTheAirUtils, $scope, $mdDialog, $location, $mdToast ) {
 
 		var vm = this;
 
@@ -83,22 +83,46 @@
 		// controller initialize
 		function _init() {
 			$log.info( '$$____ :: CONTROLLER INITIALIZE', 'NetworksCtrl' );
-			var p1, p2, p3;
-			// get network shows if route param is available else list networks
-			if( $routeParams.networkId ) {
-				p2 = vm.getNetworkShows();
-				p3 = vm.getNetworkDetails();
-			}
-			else {
-				p1 = vm.getNetworks();
-			}
-			// hide progress bar
-			$q
-				.all( [ p1, p2, p3 ] )
-				.then( function() {
-					vm.pageLoading = false;
-				} );
-		}
+      vm.appState = OnTheAirUtils.getAppState();
+      $scope.$watchCollection( 'vm.appState.user', function( newVal, oldVal ) {
+        if( false === newVal && false === oldVal ) {
+          // skip initial $watch visit
+          return;
+        }
+        else if( newVal ) {
+    			var p1, p2, p3;
+    			// get network shows if route param is available else list networks
+    			if( $routeParams.networkId ) {
+    				p2 = vm.getNetworkShows();
+    				p3 = vm.getNetworkDetails();
+    			}
+    			else {
+    				p1 = vm.getNetworks();
+    			}
+    			// hide progress bar
+    			$q
+    				.all( [ p1, p2, p3 ] )
+    				.then( function() {
+    					vm.pageLoading = false;
+    				} );          
+        }
+        else {
+          $mdDialog
+            .show( $mdDialog.confirm( {
+              title: 'Uppsiee!',
+              textContent: 'You should "login" to see saved networks list',
+              ok: 'Login',
+              cancel: 'no thanks'
+            } ) )
+            .then( function() {
+              $location.path( '/welcome' );
+            }, function() {
+              $mdToast.showSimple( 'Maybe later?' );
+              $location.path( '/' );
+            } );
+        }
+      } );
+    }
 
 	}
 
